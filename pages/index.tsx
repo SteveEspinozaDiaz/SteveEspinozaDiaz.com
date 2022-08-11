0dd98e82c41d9c3cfff9df1370480c4a5351886a
SteveEspinozaDiaz <Steve_EspinozaDiaz@student.uml.edu>
2022-08-10 21:31:21 -0400

    Reformat with Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -8,13 +8,19 @@ const Home: NextPage = () => {
       <Head>
         <title>Home | Steve Espinoza Diaz</title>
         <meta name="author" content="Steve Espinoza Diaz" />
-        <meta name="description" content="Personal website for Steve Espinoza Diaz" />
-        <meta name="keywords" content="Steve Espinoza Diaz, Physics, Math, Programming" />
+        <meta
+          name="description"
+          content="Personal website for Steve Espinoza Diaz"
+        />
+        <meta
+          name="keywords"
+          content="Steve Espinoza Diaz, Physics, Math, Programming"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className={styles['main-title']}>SteveEspinozaDiaz.com</h1>
+      <h1 className={styles["main-title"]}>SteveEspinozaDiaz.com</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
