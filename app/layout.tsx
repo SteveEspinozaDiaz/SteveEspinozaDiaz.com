82416641dd2326a2ade33d6e65267cfafea113ce
SteveEspinozaDiaz <Steve_EspinozaDiaz@student.uml.edu>
2023-05-14 19:16:27 -0400

    Add meta viewport tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({children,}: {children: React.ReactNode}) {
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/stackoverflow-dark.min.css" integrity="sha512-Xn1b0y/BrCD7usnEh6r9CcKxHXFVleVUjGDnfc95zDDwFUwtOz3lJC/XtJcuLRNyrMQJEEToFfwjC9Ue/aWY/g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />        
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/katex.min.css" integrity="sha384-3UiQGuEI4TTMaFmGIZumfRPtfKQ3trwQE2JgosJxCnGmQpL/lJdjpcHkaaFwHlcI" crossOrigin="anonymous"></link>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </head>
       <body className={`${inter.className} text-slate-950 dark:text-slate-50 bg-yellow-500`}>
         <HeaderMain />
