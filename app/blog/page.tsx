cc1612e1ccc45d25f77e0760368f5cb06106da67
SteveEspinozaDiaz <Steve_EspinozaDiaz@student.uml.edu>
2023-05-14 19:04:07 -0400

    Use relative path for getMetadata

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,36 @@
 import WrapperMain from "@/data/components/layout/WrapperMain";
 import React from "react";
 
-import { getMetadata, getSlugs } from "@/utils/mdx-utils";
+// import { getMetadata, getSlugs } from "@/utils/mdx-utils";
+import fs from "fs"
+import matter from "gray-matter";
+
 import Link from "next/link";
 
 import ComputerDesktopSolidIcon from "@/public/assets/vector/icons/ComputerDesktopSolid";
 import WrapperMainTitle from "@/data/components/layout/WrapperMainTitle";
 
+export const getMetadata = (): any => {
+  const pageFiles = fs.readdirSync("./data/pages/tutorials")
+  const mdxPages = pageFiles.filter((file) => file.endsWith(".mdx"))
+
+  const posts: any = mdxPages.map((fileName) => {
+      const fileContents = fs.readFileSync(`./data/pages/tutorials/${fileName}`, 'utf8')
+      const matterResult = matter(fileContents);
+      return {
+          title: matterResult.data.title,
+          date: matterResult.data.date,
+          subtitle: matterResult.data.subtitle,
+          slug: fileName.replace(".mdx", ""),
+      }
+  })
+
+  return posts;
+}
+
 function BlogPage() {  
 
-  const metaData = getMetadata("../app/data/pages/tutorials/")
+  const metaData = getMetadata()
   const postComponents = metaData.map((metaDatum: any, index: number) => (
     <div key={`tutorial-${metaDatum.slug}-${index}`}>
       <Link href={`/blog/tutorials/${metaDatum.slug}`}>{metaDatum.title}</Link>
