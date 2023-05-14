81913b419ec3cc5710f87bce8be6e677a78a844e
SteveEspinozaDiaz <Steve_EspinozaDiaz@student.uml.edu>
2023-05-14 18:10:12 -0400

    Add primitive blog pages

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 import WrapperMain from "@/data/components/layout/WrapperMain";
 import React from "react";
 
-import { getSlugs } from "@/utils/mdx-utils";
+import { getMetadata, getSlugs } from "@/utils/mdx-utils";
 import Link from "next/link";
 
 import ComputerDesktopSolidIcon from "@/public/assets/vector/icons/ComputerDesktopSolid";
@@ -9,17 +9,18 @@ import WrapperMainTitle from "@/data/components/layout/WrapperMainTitle";
 
 function BlogPage() {
 
-  // const slugs = getSlugs('/app/data/pages/tutorials')
-  // const slugComponents = slugs.map((slug) => {return(
-  // <div>
-  //   <Link href={`/blog/tutorials/${slug}`}>{slug}</Link>
-  // </div>
-  // )})
+  const metaData = getMetadata('/app/data/pages/tutorials/')
+  const postComponents = metaData.map((metaDatum: any, index: number) => (
+    <div key={`tutorial-${metaDatum.slug}-${index}`}>
+      <Link href={`/blog/tutorials/${metaDatum.slug}`}>{metaDatum.title}</Link>
+    </div>
+  ))
+
 
   return (<WrapperMain>
     <WrapperMainTitle title="Blog" Icon={ComputerDesktopSolidIcon}/>
-    <h2>Coming Soon...</h2>
-    {/* {slugComponents} */}
+    <h2>All Posts</h2>
+    {postComponents}
   </WrapperMain>);
 }
 
--- utils/mdx-utils.ts
+++ utils/mdx-utils.ts
@@ -1,4 +1,6 @@
 import fs from 'fs'
+import matter from 'gray-matter'
+import { bundleMDX } from 'mdx-bundler'
 
 export const getSlugs = (dataFolder: string = '/app/data/pages/') => {
     const pageFiles = fs.readdirSync(dataFolder)
@@ -12,3 +14,23 @@ export const getContent = (slug: string, dataFolder:string = '/app/data/pages/')
     const fileContents = fs.readFileSync(mdxFile, "utf8")
     return fileContents
 }
+
+export const getMetadata = (dataFolder: string = 'app/data/pages/'): any => {
+    const pageFiles = fs.readdirSync(dataFolder)
+    const mdxPages = pageFiles.filter((file) => file.endsWith(".mdx"))
+
+    const posts: any = mdxPages.map((fileName) => {
+        const fileContents = fs.readFileSync(`${dataFolder}${fileName}`, 'utf8')
+        const matterResult = matter(fileContents);
+        return {
+            title: matterResult.data.title,
+            date: matterResult.data.date,
+            subtitle: matterResult.data.subtitle,
+            slug: fileName.replace(".mdx", ""),
+        }
+    })
+
+    return posts;
+}
+
+
